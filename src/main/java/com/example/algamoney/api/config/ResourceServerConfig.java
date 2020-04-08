9635d08eaa8a8d054f4fd1078f393b0f44e4c439
Pierre Duque <pierreduque@gmail.com>
2020-04-08 14:55:07 -0300

    Aula 7.4 v1

---
+++ src/main/java/com/example/algamoney/api/config/ResourceServerConfig.java
@@ -0,0 +1,57 @@
+package com.example.algamoney.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
+
+@Configuration
+//@EnableWebSecurity
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+	
+//	@Autowired
+//	private UserDetailsService userDetailsService;
+	
+//	@Autowired
+//	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		/**Após implemnetar o usuario no banco de dados não precisa mais da implementação em memoria*/
+	//	auth.inMemoryAuthentication()
+	//		.withUser("admin").password("admin").roles("ROLE");
+//		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+//	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/categorias").permitAll()	// Todo mundo tem acesso
+				.anyRequest().authenticated()
+				.and()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+			.csrf().disable();
+	}
+	
+	
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.stateless(true);
+	}
+	
+	@Bean
+	public MethodSecurityExpressionHandler createExpressionHandler() {
+		return new OAuth2MethodSecurityExpressionHandler();
+	}
+
+//	public PasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
+
+}
